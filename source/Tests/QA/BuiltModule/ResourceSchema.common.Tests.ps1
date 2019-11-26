bfd07d52145ddd211f3df0cb48d28c4abcacd561
gaelcolas <gaelcolas@synedgy.com>
2019-11-26 14:03:40 +0800

    making test skip test-xdscresource when not running as admin or not on windows

--- source/Tests/QA/BuiltModule/ResourceSchema.common.Tests.ps1
+++ source/Tests/QA/BuiltModule/ResourceSchema.common.Tests.ps1
@@ -6,21 +6,28 @@ param (
     $ProjectPath,
     $SourceManifest
 )
-Describe 'Common Tests - Script Resource Schema Validation' {
-    Import-Module -Name xDscResourceDesigner -ErrorAction Stop
-    $dscResourcesFolderFilePath = Join-Path -Path $ModuleBase -ChildPath 'DscResources'
 
+
+$Principal = [Security.Principal.WindowsPrincipal]::new([Security.Principal.WindowsIdentity]::GetCurrent())
+$isAdmin = $Principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+Describe 'Common Tests - Script Resource Schema Validation' -Tag WindowsOnly {
+    if ($isAdmin -and ($IsWindows -or $PSVersionTable.PSEdition -eq 'Desktop'))
+    {
+        Import-Module -Name xDscResourceDesigner -ErrorAction Stop
+    }
+
+    $dscResourcesFolderFilePath = Join-Path -Path $ModuleBase -ChildPath 'DscResources'
     $scriptResourceNames = Get-ModuleScriptResourceName -ModulePath $ModuleBase
     foreach ($scriptResourceName in $scriptResourceNames)
     {
         Context $scriptResourceName {
             $scriptResourcePath = Join-Path -Path $dscResourcesFolderFilePath -ChildPath $scriptResourceName
 
-            It 'Should pass Test-xDscResource' {
+            It 'Should pass Test-xDscResource' -Skip:(!$isAdmin -or !($IsWindows -or $PSVersionTable.PSEdition -eq 'Desktop')) {
                 Test-xDscResource -Name $scriptResourcePath | Should -Be $true
             }
 
-            It 'Should pass Test-xDscSchema' {
+            It 'Should pass Test-xDscSchema' -Skip:(!$isAdmin -or !($IsWindows -or $PSVersionTable.PSEdition -eq 'Desktop')) {
                 $mofSchemaFilePath = Join-Path -Path $scriptResourcePath -ChildPath "$scriptResourceName.schema.mof"
                 Test-xDscSchema -Path $mofSchemaFilePath | Should -Be $true
             }
