f39fc6265c999620008ecb815192554a79ec0873
Johan Ljunggren <johan.ljunggren@shamn.se>
2021-01-07 16:38:29 +0100

    Fix problem with WPS in MarkdownLinks.common

--- source/Tests/QA/MarkdownLinks.common.v5.Tests.ps1
+++ source/Tests/QA/MarkdownLinks.common.v5.Tests.ps1
@@ -72,16 +72,16 @@ BeforeDiscovery {
     $markdownFileFilter = '*.md'
 
     # This will fetch files in the root of the project (repository) folder.
-    $markdownFiles = Get-ChildItem -Path $ProjectPath -File -Filter $markdownFileFilter
+    $markdownFiles = Get-ChildItem -Path $ProjectPath -Filter $markdownFileFilter
 
     # This will recursively fetch all files in the built module's folder.
-    $markdownFiles += Get-ChildItem -Path $ModuleBase -File -Recurse -Filter $markdownFileFilter | `
+    $markdownFiles += Get-ChildItem -Path $ModuleBase -Recurse -Filter $markdownFileFilter | `
         WhereModuleFileNotExcluded
 
     if ($SourcePath)
     {
         # This will recursively fetch all files in the source folder.
-        $markdownFiles += Get-ChildItem -Path $SourcePath -File -Recurse -Filter $markdownFileFilter | `
+        $markdownFiles += Get-ChildItem -Path $SourcePath -Recurse -Filter $markdownFileFilter | `
             WhereSourceFileNotExcluded
     }
 
