1143c5037899d81835e503f366aeea1b7a3be3c4
Johan Ljunggren <johan.ljunggren@shamn.se>
2021-02-08 16:52:50 +0100

    Fix correct alias against right filename

--- source/Public/Task.Fail_Build_If_HQRM_Tests_Failed.ps1
+++ source/Public/Task.Fail_Build_If_HQRM_Tests_Failed.ps1
@@ -1,15 +1,16 @@
 <#
     .SYNOPSIS
-        This is the alias to the build task Invoke_HQRM_Tests's script file.
+        This is the alias to the build task Fail_Build_If_HQRM_Tests_Failed's
+        script file.
 
     .DESCRIPTION
-        This makes available the alias 'Task.Invoke_HQRM_Tests' that is exported
-        in the module manifest so that the build task can be correctly imported
-        using for example Invoke-Build.
+        This makes available the alias 'Task.Fail_Build_If_HQRM_Tests_Failed' that
+        is exported in the module manifest so that the build task can be correctly
+        imported using for example Invoke-Build.
 
     .NOTES
         This is using the pattern lined out in the Invoke-Build repository
         https://github.com/nightroman/Invoke-Build/tree/master/Tasks/Import.
 #>
 
-Set-Alias -Name 'Task.Invoke_HQRM_Tests' -Value "$PSScriptRoot/tasks/Invoke_HQRM_Tests.build.ps1"
+Set-Alias -Name 'Task.Fail_Build_If_HQRM_Tests_Failed' -Value "$PSScriptRoot/tasks/Fail_Build_If_HQRM_Tests_Failed.build.ps1"
